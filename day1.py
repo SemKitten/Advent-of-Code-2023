7bf9259f8153e4e2ec5c09278b02038ec50bcfb8
SemKitten <30296787+SemKitten@users.noreply.github.com>
2023-12-01 17:29:44 -0300

    Day 1 challenge
    Resolution of Day 1 challenge in AoC 2023.

---
+++ day1.py
@@ -0,0 +1,26 @@
+import regex as re
+
+with open ('input.txt') as f:
+    lines = f.read().split()
+
+digit_words = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
+def getNumbers (line):
+    def getDigit(target):
+        return str(digit_words.index(target)+1) if target in digit_words else target
+
+    matches = re.findall(r'\d|'+'|'.join(digit_words), line, overlapped=True)
+    first_digit = matches[0]
+    last_digit = matches[-1]
+
+    first_digit = getDigit(first_digit)
+    last_digit = getDigit(last_digit)
+
+    return int(first_digit + last_digit)
+
+numbers = []
+
+for x in lines:
+    numbers.append(getNumbers(x))
+
+print(sum(numbers))
